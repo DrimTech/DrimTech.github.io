bc3891a551ab48d77e8e3d9185f8f748da93364d
Enrique Ochoa <73363057+Jk99z2@users.noreply.github.com>
2021-04-27 14:18:46 -0500

    Actualización Database Connect
    Se actualiza el código php que anteriormente conectaba a la base de datos, usando PDO.

---
+++ DBconect.php
@@ -0,0 +1,17 @@
+<?php
+$db_host="localhost"; //localhost server 
+$db_user="root";	// usuario de la base de datos
+$db_password="root";	// contraseña del usuario de la base de datos
+$db_name="php_multiplelogin";	// nombre de la base de datos
+
+try
+{
+	$db=new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password); // Se establece la conexión con la database
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOEXCEPTION $e)
+{
+	$e->getMessage();
+}
+
+?>

d1a8932af5c9022065f73a0a36206dbb24a687ef
Enrique Ochoa <73363057+Jk99z2@users.noreply.github.com>
2021-05-24 14:14:24 -0500

    Conversión de secciones a PHP

---
+++ secciones/noticias.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>DrimTech - Blog de Tecnologia</title>
+
+    <link rel="icon" href="browser.png">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../asets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="../assets/css/Article-List.css">
+    <link rel="stylesheet" href="../assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="../assets/css/Highlight-Phone.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu-1.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head><!--Título de pestaña, importaciones y conexiones-->
+
+<body style="color: var(--blue);background: var(--gray-dark);">
+
+    <h1 style="border-right-style: none;text-align: center;color: var(--light);">DrimTech</h1><!--Título-->
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="background: var(--gray);">
+        <div class="container"><a class="navbar-brand" href="../index.html">DrimTech Blog</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="noticias.php" style="color: #509AC9;">Noticias</a></li>
+                    <li class="nav-item"><a class="nav-link" href="hardware.php">Hardware</a></li>
+                    <li class="nav-item"><a class="nav-link" href="software.php">Software<br></a></li>
+                    <li class="nav-item"><a class="nav-link" href="otros.php">Otros<br></a></li>
+                </ul><!--Lista de elementos de la Navigation Bar (Barra de navegación)-->
+                <form class="form-inline mr-auto" target="_self">
+                    <div class="form-group"><label for="search-field"></label></div>
+                </form>
+                
+            </div>
+        </div>
+    </nav><!--Barra de navegación/elementos enlazables-->
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+        <footer class="footer-basic" style="background: var(--gray);border-color: var(--orange);">
+                      
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" >Home</a></li>
+                <li class="list-inline-item"><a href="#" >Servicios</a></li>
+                <li class="list-inline-item"><a href="#" >Sobre nosotros</a></li>
+                <li class="list-inline-item"><a href="#" >Términos</a></li>
+                <li class="list-inline-item"><a href="#" >Política de privacidad</a></li>
+            </ul><!--Términos y condiciones, servicios, sobre nosotros, privacidad-->
+            
+            <div class="text-center">
+            <br>
+            <p class="copyright" style="display:inline; margin-left: 0px; text-align: center;"><a href="./index.php" style="color: #aaa; text-decoration: none;">D</p></a><p class="copyright" style="display: inline; margin-right:0px">rimTeam Technlogies© 2021, All Rights Reserved</p>
+            <div>
+
+        </footer><!--Footer, pie de página, redes sociales y contacto-->
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/Sidebar-Menu.js"></script>
+    
+</body>
+</html>

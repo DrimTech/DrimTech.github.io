bbf898a9595534046f092ffeb1278615d65ab2b8
Enrique Ochoa <73363057+Jk99z2@users.noreply.github.com>
2021-05-24 14:17:52 -0500

    Adición de DB completas.
    Se añade el archivo que contiene las dos databases, y se elimina el registro de la única que se había registrado.

--- database/databases_blog.sql
+++ database/databases_blog.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 02-05-2021 a las 03:49:02
--- Versión del servidor: 5.7.24
--- Versión de PHP: 7.4.1
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-05-2021 a las 21:10:09
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,58 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
+-- Base de datos: `blog_posts`
+--
+CREATE DATABASE IF NOT EXISTS `blog_posts` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `blog_posts`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contenido`
+--
+
+CREATE TABLE `contenido` (
+  `id` int(11) NOT NULL,
+  `Titulo` varchar(25) NOT NULL,
+  `Fecha` datetime NOT NULL,
+  `Comentario` text NOT NULL,
+  `Imagen` varchar(35) NOT NULL,
+  `categoria` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `contenido`
+--
+
+INSERT INTO `contenido` (`id`, `Titulo`, `Fecha`, `Comentario`, `Imagen`, `categoria`) VALUES
+(1, 'Test', '2021-05-24 20:50:41', 'Texto de ejemplo.', '954742.jpg', 'noticias'),
+(2, 'Segundo post de ejemplo', '2021-05-24 20:57:12', 'Ejemplo de texto 2.', 'wallpapertip_galaxy-note-3-wallpape', 'hardware');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- Base de datos: `php_multiplelogin`
 --
+CREATE DATABASE IF NOT EXISTS `php_multiplelogin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `php_multiplelogin`;
 
 -- --------------------------------------------------------
 
